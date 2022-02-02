540a89645702b36352abb6406fcd7e4c26be9b50
David Barragán Merino <david.barragan@kaleidos.net>
2022-02-02 13:33:42 +0100

    fix(team): use the new profiles urls

--- src/views/Team/Team.vue
+++ src/views/Team/Team.vue
@@ -8,12 +8,12 @@ export default defineComponent({
   data () {
     return {
       members: [
-        { name: 'Marina López', position: 'Front-End Developer', link: 'https://kaleidos.net/FF8A80/' },
-        { name: 'Natacha Menjibar', position: 'UX/UI Designer', link: 'https://kaleidos.net/492858/' },
-        { name: 'Esther Moreno', position: 'Creator & Illustrator', link: 'https://kaleidos.net/720B34/' },
-        { name: 'Yamila Moreno', position: 'Back-End Developer', link: 'https://kaleidos.net/D70A53/' },
-        { name: 'Ramiro Sánchez', position: 'Front-End Developer', link: 'https://kaleidos.net/E32841/' },
-        { name: 'David Barragán', position: 'Naive Developer', link: 'https://kaleidos.net/FFF8E7/' }
+        { name: 'Marina López', position: 'Front-End Developer', link: 'https://kaleidos.net/kaleiders/FF8A80/' },
+        { name: 'Natacha Menjibar', position: 'UX/UI Designer', link: 'https://kaleidos.net/kaleiders/492858/' },
+        { name: 'Esther Moreno', position: 'Creator & Illustrator', link: 'https://kaleidos.net/kaleiders/720B34/' },
+        { name: 'Yamila Moreno', position: 'Back-End Developer', link: 'https://kaleidos.net/kaleiders/D70A53/' },
+        { name: 'Ramiro Sánchez', position: 'Front-End Developer', link: 'https://kaleidos.net/kaleiders/E32841/' },
+        { name: 'David Barragán', position: 'Naive Developer', link: 'https://kaleidos.net/kaleiders/FFF8E7/' }
       ]
     }
   }
